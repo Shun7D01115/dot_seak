81726eac41ffde4d67dceabbed98b8fda43bda3d
[Shun7D01115] <[shunki.1ban.soccer@gmail.com]>
2022-08-29 11:49:18 +0900

    a

--- dot_seak.py
+++ dot_seak.py
@@ -7,7 +7,10 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import math
 
-import os,tkinter,tkinter.filedialog,tkinter.messagebox
+import os
+import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
 from tkinter import messagebox
 
 """
@@ -36,41 +39,47 @@ from tkinter import messagebox
     ナンバリング画像を出力したい!!!
 """
 
+
 def file_read():
     #解析ファイル読み込み
-    fTyp = [("Data file","*")]
-    iDir =os.path.abspath(os.path.dirname(__file__))
-    file_name = tkinter.filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
-    return(file_name)
+    fTyp = [("Data file", "*")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    file_name = tkinter.filedialog.askopenfilename(
+        filetypes=fTyp, initialdir=iDir)
+    return (file_name)
+
 
 def Contours(img_gray):
 
-    labels,contours = cv2.findContours(img_gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    labels, contours = cv2.findContours(
+        img_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     Areas = []
     Diameters = []
     Density = []
 
+
 def none(x):
     pass
+
 
 def Trackbar():
     #閾値変化トラックバー
     cv2.namedWindow("Threshold")
-    cv2.resizeWindow("Threshold",640,240)
-    cv2.createTrackbar("Gaussian","Threshold",100,255,none)
-    cv2.createTrackbar("Minus","Threshold",0,255,none)
+    cv2.resizeWindow("Threshold", 640, 240)
+    cv2.createTrackbar("Low", "Threshold", 100, 255, none)
+
 
 #画像のグレースケール
 path = file_read()
-file_name,ext = os.path.splitext(os.path.basename(path))
+file_name, ext = os.path.splitext(os.path.basename(path))
 
 Trackbar()
 
 
 img = cv2.imread(path)
-img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-kernel = np.ones((5,5),np.uint8)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+kernel = np.ones((5, 5), np.uint8)
 img_copy = img.copy()
 
 while True:
@@ -80,23 +89,25 @@ while True:
     if cv2.waitKey(1) == 13:
         break
 
-    gaussian = cv2.getTrackbarPos("Gaussian","Threshold")
-    minus = cv2.getTrackbarPos("Minus","Threshold")
-    img_bit = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,gaussian,minus)
-    contours,hierarchy = cv2.findContours(img_bit,cv2.FETR_LIST,cv2.CHAIN_APPROX_NONE)
+    threshold = cv2.getTrackbarPos("Low", "Threshold")
+    ret, img_bit = cv2.threshold(img_gray, threshold, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(
+        img_bit, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    #th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
 
-    #エッジ等の描画処理
-    for i in range(0,len(contours)):
+    for i in range(0, len(contours)):
         if len(contours[i]) > 0:
             if cv2.contourArea(contours[i]) < 500:
                 continue
             rect = contours[i]
-            x,y,w,h = cv2.boundingRect(rect)
-            cv2.polylines(img_copy,contours[i],True,(255,0,0),1)
-            cv2.rectangle(img_copy,(x,y),(x+w,y+h),(0,255,0),1)
+            x, y, w, h = cv2.boundingRect(rect)
+            cv2.polylines(img_copy, contours[i], True, (255, 0, 0), 1)
+            cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
-    cv2.imshow("img_th",img_copy)
+    cv2.imshow("img_th", img_copy)
 
-cv2.imshow("img_exp",img_copy)
+##branchできてるかな?
+
+
 cv2.waitKey()
 cv2.destroyAllWindows()
